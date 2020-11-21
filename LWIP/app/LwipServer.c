a800f08cd50d7de510df42e1e06a63a1c35d3ef7
Lighter-z <1106606588@qq.com>
2020-11-21 19:55:20 +0800

    解决tcp server例程不能进行数据传输 原因：在数据拷贝时，不需要进临界区。这里使用的方式与正点原子的不相同，因此不需要进去临界区。

--- LWIP/app/LwipServer.c
+++ LWIP/app/LwipServer.c
@@ -18,8 +18,7 @@ static void LwipServerThread(void *param) {
   
   conn = netconn_new(NETCONN_TCP);
   netconn_bind(conn, IP_ADDR_ANY, TCP_SERVER_PORT);
-  netconn_listen(conn);
-//  conn->recv_timeout = 10;    //³¬Ê±Ê±¼ä
+  netconn_listen(conn); 
   printf("tcp server thread");
   
   while(1) {
@@ -27,28 +26,22 @@ static void LwipServerThread(void *param) {
     if(err == ERR_OK) {
       struct netbuf *net_buf;
       void *data;
-      u16_t len;
-//      new_conn->recv_timeout = 10;
-      while((err = netconn_recv(new_conn, &net_buf)) == ERR_OK) {
-        OS_ENTER_CRITICAL();
+      u16_t len; 
+      while((err = netconn_recv(new_conn, &net_buf)) == ERR_OK) { 
         do{
           netbuf_data(net_buf, &data, &len);
           err = netconn_write(new_conn, data, len, NETCONN_COPY);
           if(err != ERR_OK) {
             printf("netconn_write :error \"%s \" \r\n", lwip_strerr(err));
           }
-        }while(netbuf_next(net_buf) >= 0);
-        OS_ENTER_CRITICAL();
+        }while(netbuf_next(net_buf) >= 0); 
         netbuf_delete(net_buf);
       }
       netconn_close(new_conn);
       netconn_delete(new_conn);
     }
-  }
-  
-  
-}
-
+  } 
+}  
 
 INT8U LwipServerThreadInit(void) {
   INT8U res;
